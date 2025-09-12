9cd0dc82a789aa663b29d67417440238ea376f1f
Kauan <reykauan6@gmail.com>
2025-09-12 20:46:26 -0300

    Consertando a passagem das minhas imagens

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const repoName = 'manicure-website-code' // nome do seu repo
 const nextConfig = {
-  output: "export",
-}
-
-export default nextConfig
+  output: 'export',
+  basePath: '/' + repoName,
+  assetPrefix: '/' + repoName + '/',
+};
+module.exports = nextConfig;
